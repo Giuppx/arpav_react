d05b3294c72ce71dceb880edb36ab0632b728da3
Giuseppe Lomasto <g.lomasto.94@gmail.com>
2025-07-23 10:27:56 +0200

    implementato routing di base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+
+import Navbar from "./navbar/Navbar";
+import Footer from "./footer/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Homepage />} />
+				{/* <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+				<Route path="/amministrazione" element={<Administration />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="#" element={<PageInProgress />} />
+				<Route path="*" element={<Notfound />} /> */}
+			</Routes>
+			<Footer />
+		</BrowserRouter>
+	);
 }
 
 export default App;
